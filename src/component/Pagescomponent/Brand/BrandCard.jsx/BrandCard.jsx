a30c966b1dbecb442c1e6a6fa633152e8db82540
daudahme <hassam.mughal2001@gmail.com>
2025-03-17 00:48:26 +0500

    remain unchanged

--- src/component/Pagescomponent/Brand/BrandCard.jsx/BrandCard.jsx
+++ src/component/Pagescomponent/Brand/BrandCard.jsx/BrandCard.jsx
@@ -20,7 +20,7 @@ const BrandCard = () => {
                 <p >5,824  <span className='text-[#D9D9D9]'>Fresh Campaigns from this Brand Group</span></p>
             </div>
             <div className='flex flex-col justify-start items-start gap-5'>
-                <div className='flex justify-between items-center gap-5'>
+                <div className='flex justify-center items-center gap-5'>
                     <div>
                         <img src={carcel} alt="" height={40} width={40} className='rounded-full'/>
                     </div>
@@ -28,11 +28,11 @@ const BrandCard = () => {
                         <h1>Nectar Sleep</h1>
                         <p> 1,718 new ads</p>
                     </div>
-                    <div className=''>
+                    <div className='ml-9'>
                         <NavBadges text={"Competitor"} Icon={TrackBrand} selected={true} />
                     </div>
                 </div>
-                <div className='flex justify-between items-center gap-5 '>
+                <div className='flex justify-center items-center gap-5 '>
                     <div >
                         <img src={rector} alt="" height={40} width={40}  className='rounded-full'/>
                     </div>
@@ -41,11 +41,11 @@ const BrandCard = () => {
                         <p>2,786 new ads</p>
 
                     </div>
-                    <div className=''>
+                    <div className='ml-9'>
                         <NavBadges text={"Competitor"} Icon={TrackBrand}  selected={true}/>
                     </div>
                 </div>
-                <div className='flex justify-between items-center gap-5' >
+                <div className='flex justify-center items-center gap-5' >
                     <div>
                         <img src={purple} alt="" height={40} width={40}  className='rounded-full'/>
                     </div>
@@ -53,7 +53,7 @@ const BrandCard = () => {
                         <h1> Casper</h1>
                         <p> 1,320 new ads</p>
                     </div>
-                    <div className=''>
+                    <div className='ml-9'>
                         <NavBadges text={"Followed"} Icon={Heart}  selected={true}/>
                     </div>
                 </div>
